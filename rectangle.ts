1944e4b87fa1212ded01dd9587399617ef525889
hai <94nguyenduyhai1819@gmail.com>
2025-09-20 09:20:30 +0700

    Add rectangle.ts file

---
+++ rectangle.ts
@@ -0,0 +1,18 @@
+
+type RectangleResult = {
+  perimeter: number;
+  area: number;
+};
+
+function calculateRectangle(width: number, height: number): RectangleResult {
+  const perimeter = 2 * (width + height);
+  const area = width * height;
+
+  return {
+    perimeter,
+    area
+  };
+}
+
+const result = calculateRectangle(5, 10);
+console.log(`Chu vi: ${result.perimeter}, Diện tích: ${result.area}`);
